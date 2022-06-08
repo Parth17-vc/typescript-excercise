148c75e14e629afd44d9e995d08b91037dc2b8ba
Parth17-vc <parth.joshi@viitor.cloud>
2022-06-08 15:18:24 +0530

    update new changes

--- task-3.js
+++ task-3.js
@@ -1,7 +1,17 @@
 //Write a typescript program to print Even numbers in given array[13,23,12,45,22,48,66,100]
 
+
+  var arr = [13,23,12,45,22,48,66,100];
+
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 === 0) {
+               console.log(arr[i]);
+            }
+        }
+
   
-  let a  = [13,23,12,45,22,48,66,100];
+// Practices for odd number
+let a  = [13,23,12,45,22,48,66,100];
  
   
   for (var i = 0; i < a.length; i++) {
--- task-4.js
+++ task-4.js
@@ -1,10 +1,14 @@
 //Write a program to delete all occurrence of element in given array [23,56,4,78,5,63,45,210,56];
 
-let arr = [23,56,4,78,5,63,45,210,56];
-let removed = arr.splice(0);
-console.log("element deleted : " + removed);
-console.log("Remaining elements in array : " + arr);
+let elements = [23,56,4,78,5,63,45,210,56];
+console.log("before splice array :" + elements);
+for (let i = 0; i < elements.length; i++) {
+  
+    elements.splice(i, elements.length);
+}
+console.log("after splice array :" + elements);
 
 //output : 
-// "element deleted : 23,56,4,78,5,63,45,210,56"
-// "Remaining elements in array : "
+
+// "before splice array : 23,56,4,78,5,63,45,210,56"
+// "after splice array :"
